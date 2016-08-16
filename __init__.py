256294b97de4142c386a33f40cdd00b0df375851
TheLastProject <iamsylvie@openmailbox.org>
2016-08-16 11:47:55 +0200

    Remove useless import

--- __init__.py
+++ __init__.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from subprocess import Popen, PIPE
 from emoji import unicode_codes
 
 from pext_base import ModuleBase
